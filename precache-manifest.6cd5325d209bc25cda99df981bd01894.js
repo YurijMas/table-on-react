ee96e40ed24b3dde965b02f78a576cd5a51f1b30
Yuri Maslennikov <yurij-maslennikov@yandex.ru>
2020-03-04 00:09:26 +0300

    Updates

---
+++ precache-manifest.6cd5325d209bc25cda99df981bd01894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21cd0365a8735b3861adf70a1993866",
+    "url": "/table-on-react/index.html"
+  },
+  {
+    "revision": "72a634d6097ec36bcc7f",
+    "url": "/table-on-react/static/css/main.55a692e8.chunk.css"
+  },
+  {
+    "revision": "6ee14c5063f13d68bee2",
+    "url": "/table-on-react/static/js/2.0af4e6f9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/table-on-react/static/js/2.0af4e6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a634d6097ec36bcc7f",
+    "url": "/table-on-react/static/js/main.25452746.chunk.js"
+  },
+  {
+    "revision": "94885d9c96a6dbf3d0fa",
+    "url": "/table-on-react/static/js/runtime-main.2ddf07bb.js"
+  }
+]);
